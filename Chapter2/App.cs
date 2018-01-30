4d8fb6c204a20c9579704157a6c46439b3b145dc
Rohit Garg <rohit@unity3d.com>
2018-01-29 16:11:55 -0800

    Complete solution, albeit unoptimized (could genericize the agents, make a better graph)

--- Chapter2/App.cs
+++ Chapter2/App.cs
@@ -11,15 +11,16 @@ namespace Chapter2
         private const int MAX_STEPS = 10000;
         private const double STARTING_REWARD = 10;
         private int _currentSteps;
-        private double _totalReward;
 
         private TestBed _tB;
-        private SampleAvgAgent _sA;
+        private SampleAvgAgent _sampleAgent;
+        private StepSizeAgent _stepAgent;
         private Graph _graph;
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
 
         private List<float> _sampleAvgRewards;
+        private List<float> _stepSizeRewards;
 
         public App()
         {
@@ -30,14 +31,16 @@ namespace Chapter2
         protected override void Initialize()
         {
             _tB = new TestBed(STARTING_REWARD);
-            _sA = new SampleAvgAgent(_tB);
+            _sampleAgent = new SampleAvgAgent(_tB);
+            _stepAgent = new StepSizeAgent(_tB);
             _graph = new Graph(graphics.GraphicsDevice, new Point(800, 600))
             {
-                Position = new Vector2(0, 300),
-                MaxValue = 5
+                Position = new Vector2(0, 600),
+                MaxValue = 20
             };
 
             _sampleAvgRewards = new List<float>();
+            _stepSizeRewards = new List<float>();
             base.Initialize();
         }
 //
@@ -52,9 +55,12 @@ namespace Chapter2
         {
             if (_currentSteps < MAX_STEPS)
             {
-                _totalReward += _sA.SampleBandit();
-                _sampleAvgRewards.Add((float)(_totalReward/_currentSteps));
+                _sampleAgent.SampleBandit();
+                _sampleAvgRewards.Add((float)(_sampleAgent.TotalReward/_currentSteps));
+                _stepAgent.SampleBandit();
+                _stepSizeRewards.Add((float)(_stepAgent.TotalReward/_currentSteps));
                 _currentSteps++;
+                Console.WriteLine((float)(_sampleAgent.TotalReward/_currentSteps) + ", " + (float)(_stepAgent.TotalReward/_currentSteps));
             }
             else
             {
@@ -68,6 +74,7 @@ namespace Chapter2
         {
             GraphicsDevice.Clear(Color.WhiteSmoke);
             _graph.Draw(_sampleAvgRewards, Color.Red);
+            _graph.Draw(_stepSizeRewards, Color.Blue);
             base.Draw(gameTime);
         }
     }
--- Chapter2/StepSizeAgent.cs
+++ Chapter2/StepSizeAgent.cs
@@ -5,18 +5,19 @@ namespace Chapter2
     /// <summary>
     /// This agent uses sample averages to pick its next action
     /// </summary>
-    public class SampleAvgAgent
+    public class StepSizeAgent
     {
         private const int NUMBER_OF_BANDITS = 10;
         private const double EPSILON = 0.1;
+        private const double STEP_SIZE = 0.1;
         private readonly TestBed _testBed;
         private readonly double[] _averages;
         private readonly int[] _numSelections;
         private readonly Random _random;
-
+        
         public double TotalReward;
         
-        public SampleAvgAgent(TestBed testBed)
+        public StepSizeAgent(TestBed testBed)
         {
             _testBed = testBed;
             _averages = new double[NUMBER_OF_BANDITS];
@@ -43,7 +44,7 @@ namespace Chapter2
             sampledValue = _testBed.Sample(banditIndex);
             _numSelections[banditIndex]++;
             _averages[banditIndex] = _averages[banditIndex] +
-                                     (1 / _numSelections[banditIndex]) * (sampledValue - _averages[banditIndex]);
+                                     STEP_SIZE * (sampledValue - _averages[banditIndex]);
             TotalReward += sampledValue;
         }
 
